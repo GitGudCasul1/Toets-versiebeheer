70998c7aedcb11b5c97dc9af69ceb6fdbb1ffda6
Jef Den Hond <jdenhond@ION.local>
2019-11-26 11:08:44 +0100

    printarray method

--- ArrayPrinter.java
+++ ArrayPrinter.java
@@ -1,9 +1,14 @@
 public class ArrayPrinter{
 	public static void main(String[] args) {
 		ArrayPrinter praktijk = new ArrayPrinter();
-      		int[] array = new int[]{1, 2, 3, 4, 5};		
+      		int[] array = new int[]{1, 2, 3, 4, 5};
+		praktijk.printarray();		
 	}
-	private void printarray(){}
+	private void printarray(){
+		for(int i = 0; i < 5; i++){
+			System.out.println(array[i])
+		}
+	}
 }
 
 
